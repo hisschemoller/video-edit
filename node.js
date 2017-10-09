b5fcbc082b00d5078161d8682d1185e428081c4b
Wouter Hisschemöller <wouter.hisschemoller@digitaslbi.com>
2017-10-09 12:48:48 +0200

    Setup for node.js express module.

--- node.js
+++ node.js
@@ -1,4 +1,5 @@
-var app = require('express')(),
+var express = require('express'),
+    app = express(),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server),
     fs = require('fs');
@@ -6,12 +7,15 @@ var app = require('express')(),
 server.listen(3000);
 
 app.get('/', function (req, res) {
-    res.sendfile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/web/index.html');
 });
+
+app.use(express.static('web'));
 
 io.sockets.on('connection', function (socket) {
     socket.on('render-frame', function (data) {
-        data.file = data.file.split(',')[1]; // Get rid of the data:image/png;base64 at the beginning of the file data
+        // Get rid of the data:image/png;base64 at the beginning of the file data
+        data.file = data.file.split(',')[1]; 
         var buffer = new Buffer(data.file, 'base64');
         fs.writeFile(__dirname + '/tmp/frame-' + data.frame + '.png', buffer.toString('binary'), 'binary');
     });
