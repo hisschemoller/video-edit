bfaa99351c3784d27b12591da4699c3920d36e5f
Wouter Hisschemöller <wouter.hisschemoller@digitaslbi.com>
2017-10-19 17:43:04 +0200

    Setup of data in musical timing.

---
+++ web/static/js/data.js
@@ -0,0 +1,24 @@
+var WH = WH || {};
+
+(function(WH) {
+    
+    let clipData = {
+        timing: 'music', // 'music|seconds'
+        ppqn: 24,
+        bpm: 120,
+        timesignature: {
+            numerator: 4,
+            denominator: 4
+        }
+    };
+    
+    clipData = clipData.concat([{
+        start: '0:0:0', end: '0:1:0',
+        clipStart: '24:0:0',
+        x: 0, xx: 100,
+        y: 200, yy: 360
+    }];
+    
+    WH.clipData = clipData;
+
+})(WH);
