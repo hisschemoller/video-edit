1a8d3cdb7e6528e614466157442e8dc024daf2fd
Wouter Hisschemöller <wouter.hisschemoller@gmail.com>
2017-11-29 20:49:27 +0100

    Render clips from old to new.

--- web/static/js/clips.js
+++ web/static/js/clips.js
@@ -8,34 +8,36 @@ var WH = WH || {};
             numClips = 8,
             clips = [],
             clipIndex = 0,
-            
+
             init = function() {
                 for (var i = 0; i < numClips; i++) {
                     clips.push(WH.createClip());
                 }
             },
-            
+
             startClips = function(clipData, isCapture) {
                 for (let i = 0, n = clipData.length; i < n; i++) {
                     clips[clipIndex].start(clipData[i], isCapture);
                     clipIndex = (clipIndex + 1) % numClips;
                 }
             },
-            
+
             draw = function(time, ctx) {
-                let clip;
-                for (let i = 0, n = clips.length; i < n; i++) {
-                    clip = clips[i];
+                let clip,
+                    renderIndex = clipIndex;
+                for (let i = 0, n = numClips; i < n; i++) {
+                    clip = clips[renderIndex];
                     if (clip.getIsPlaying(time)) {
                         clip.draw(ctx);
                     }
+                    renderIndex = (renderIndex + 1) %numClips;
                 }
             };
-        
+
         that = specs.that || {};
-        
+
         init();
-        
+
         that.startClips = startClips;
         that.draw = draw;
         return that;
