83ac6a29fd0199a35dc5cb06e09dcfe9af9933c4
Wouter Hisschemöller <wouter.hisschemoller@gmail.com>
2017-12-09 21:01:05 +0100

    Bug fix, in progress.

--- web/static/js/clips.js
+++ web/static/js/clips.js
@@ -33,13 +33,16 @@ var WH = WH || {};
                         clip = idleClips.pop();
                         clip.start(clipData[i], isCapture);
                         activeClips.push(clip);
+                        console.log('startClips', idleClips.length);
                     }
                 }
             },
 
             stopClips = function() {
-                idleClips.concat(stoppedClips);
-                stoppedClips.length = 0;
+                console.log('stopClips stoppedClips', stoppedClips.length);
+                idleClips = idleClips.concat(stoppedClips);
+                stoppedClips = [];
+                console.log('stopClips idleClips', idleClips.length);
             },
 
             draw = function(time, ctx) {
