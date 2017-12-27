763610a27e3a1460027db9503c2fa542075968f6
Wouter Hisschemöller <wouter.hisschemoller@digitaslbi.com>
2017-12-27 22:07:34 +0100

    Bug fixed detecting last frame in clip.

--- web/static/js/clip.js
+++ web/static/js/clip.js
@@ -57,9 +57,9 @@ var WH = WH || {};
                 if (isPlaying) {
                     if (position < data.end) {
                         let localPosition = ((position - globalStartPosition) / 1000) + data.clipStart;
-                        let newImgURLNr = Math.floor(localPosition * framerate) + 1;
+                        let newImgURLNr = (Math.floor(localPosition * framerate) + 1) % data.resource.frames;
                         if (newImgURLNr !== imgURLNr) {
-                            if (imgURLNr <= data.resource.frames) {
+                            if (newImgURLNr <= data.resource.frames) {
                                 imgURLNr = newImgURLNr;
                                 img.src = imgURLPrefix + ((imgURLNr <= 99999) ? ('0000' + imgURLNr).slice(-5) : '99999') + imgURLSuffix;
                             } else {
