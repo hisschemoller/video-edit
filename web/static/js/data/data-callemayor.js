8263b9a990f8a7b4c2da9a4cdf7c516c3a509274
Wouter Hisschemöller <wouter.hisschemoller@digitaslbi.com>
2018-05-11 16:36:58 +0200

    All video resources added and basic working data.

--- web/static/js/data/data-callemayor.js
+++ web/static/js/data/data-callemayor.js
@@ -19,6 +19,18 @@ var WH = WH || {};
         id: 'cmayor1',
         url: 'static/seq/cmayor1/frame_',
         frames: 14996
+      },{
+        id: 'cmayor2',
+        url: 'static/seq/cmayor2/frame_',
+        frames: 15483
+      },{
+        id: 'cmayor3a',
+        url: 'static/seq/cmayor3a/frame_',
+        frames: 13852
+      },{
+        id: 'cmayor3b',
+        url: 'static/seq/cmayor3b/frame_',
+        frames: 3881
       }],
       clips: []
     },
@@ -27,17 +39,34 @@ var WH = WH || {};
       let clips = [];
 
       let start = WH.util.musicToTime('0:0:0'),
-        end = WH.util.musicToTime('32:0:0'),
-        cmayor1ClipStart = 10;
+        end = WH.util.musicToTime('64:0:0');
 
-      clips = [...clips, 
+      clips = [...clips,
         {
           resourceID: 'cmayor1',
           start: start,
           end: end,
-          clipStart: cmayor1ClipStart,
-          x1: 0, x2: 640,
+          clipStart: 6,
+          x1: 0, x2: 210,
           y1: 0, y2: 480
+        },
+        {
+          resourceID: 'cmayor2',
+          start: start,
+          end: end,
+          clipStart: 46,
+          x1: 210, x2: 430,
+          y1: 0, y2: 480,
+          offsetX: 210, offsetY: 0
+        },
+        {
+          resourceID: 'cmayor3a',
+          start: start,
+          end: end,
+          clipStart: 51,
+          x1: 430, x2: 640,
+          y1: 0, y2: 480,
+          offsetX: 430, offsetY: 0
         }
       ];
 
